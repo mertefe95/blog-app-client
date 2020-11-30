1d4b2013b854ebea1e2708107f5f12408a2782d6
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-11-30 12:51:58 +0300

    register and login

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import ViewPosts from "./components/ViewPosts";
 import Footer from "./components/Footer";
-import CreatePost from "./components/CreatePost"
-import Post from "./components/Post"
+import CreatePost from "./components/CreatePost";
+import Post from "./components/Post";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
           <Route path="/" exact component={ViewPosts} />
           <Route path="/posts/:id" exact component={Post} />
           <Route path="/createpost" exact component={CreatePost} />
-          </Switch>
+          <Route path="/register" exact component={Register} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
         <Footer />
       </Router>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,6 +11,8 @@ function Nav() {
             <ul className="nav-ul">
                 <li className="nav-li-first"><a href="/">VIEW POSTS</a></li>
                 <li className="nav-li-second"><a href="/createpost">CREATE A POST</a></li>
+                <li className="nav-li-third"><a href="/register">REGISTER</a></li>
+                <li className="nav-li-fourth"><a href="/login">LOGIN</a></li>
             </ul>
 
         </nav>
