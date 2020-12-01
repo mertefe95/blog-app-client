c4902844d13b81e1cd55d1bf6da710557cb0fbea
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-02 00:18:24 +0300

    onchangehandler changed

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,37 @@
 import React, {useState} from "react";
+import Axios from "axios";
 
 const CreatePost = () => {
-    const [blogTitle, setBlogTitle] = useState('')
-    const [blogText, setBlogText] = useState('')
-    const [authorName, setAuthorName] = useState('')
-    
+    const [blogTitle, setBlogTitle] = useState('');
+    const [blogText, setBlogText] = useState('');
+    const [authorName, setAuthorName] = useState('');
+
+    const changeOnClick = e => {
+        e.preventDefault();
+
+        const post = {
+            blogTitle,
+            blogText,
+            authorName   
+        }
+
+        Axios.post('https://blog-app-mern-stack.herokuapp.com/api/posts', post)
+            .then(res => console.log(res.data))
+            .catch(err => {
+                console.log(err);
+            })
+    };
+
     return (
-    <form className="create-post-form">
+    <form onSubmit={changeOnClick} encType="multipart/form-data" className="create-post-form">
         <label htmlFor="blogtitle">Blog Title:</label>
-        <input type="text" id="blogtitle" name="blogtitle" ></input>
+        <input onChange={e => setBlogTitle(e.target.value)} type="text" id="blogtitle" name="blogtitle" ></input>
 
         <label htmlFor="blogtext">Blog Text:</label>
-        <input type="text" id="blogtext" name="blogtext"></input>
+        <input onChange={e => setBlogText(e.target.value)} type="text" id="blogtext" name="blogtext"></input>
 
         <label htmlFor="authorname">Author Name:</label>
-        <input type="text" id="authorname" name="authorname"></input>
+        <input onChange={e => setAuthorName(e.target.value)} type="text" id="authorname" name="authorname"></input>
 
         <button type="submit">Submit</button>
     </form>
