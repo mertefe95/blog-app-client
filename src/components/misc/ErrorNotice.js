11e8b70fc54f532ff23c8f09f6924d18f40efb58
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-13 19:09:45 +0300

    new changes to error button

--- src/components/misc/ErrorNotice.js
+++ src/components/misc/ErrorNotice.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 export default function ErrorNotice(props) {
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          '& > *': {
+            margin: theme.spacing(1),
+          },
+        },
+      }));
+      const classes = useStyles();
+
+      
     return (
         <div className="error-notice">
             <span>{props.message}</span>
-            <button onClick={props.clearError}>X</button>
+            <Button onClick={props.clearError} color="secondary">CLEAR MESSAGE</Button>
         </div>
         );
     
