006c27d44756fc57830219f2185cabff75ec490d
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-13 17:15:21 +0300

    all

--- src/components/Register.js
+++ src/components/Register.js
@@ -35,7 +35,7 @@ const Register = () => {
 
     try {
         const newUser = { username, email, password };
-        await Axios.post("http://localhost:8080/api/register", newUser);
+        await Axios.post("https://blog-app-mern-stack.herokuapp.com/api/register", newUser);
 
         setVerifyMessage({
         text: "Please verify your email to proceed login.",
