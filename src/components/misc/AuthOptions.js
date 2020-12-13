cb2571a1c637fe6a10a0bf4915de18abe565c10f
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-13 15:50:36 +0300

    some cmmt

--- src/components/misc/AuthOptions.js
+++ src/components/misc/AuthOptions.js
@@ -4,9 +4,6 @@ import UserContext from "../../context/UserContext";
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import NavigationIcon from '@material-ui/icons/Navigation';
 import Button from '@material-ui/core/Button';
 
 
